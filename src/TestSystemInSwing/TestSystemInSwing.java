ccc1e8a9ef62a96afcc0f14a38cbdf93c8d9bcea
Jirui Qiu <jirui.qiu15@gmail.com>
2018-03-13 01:15:37 -0500

    Rename variables

--- src/TestSystemInSwing/TestSystemInSwing.java
+++ src/TestSystemInSwing/TestSystemInSwing.java
@@ -5,47 +5,48 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TestSystemInSwing extends JFrame implements ActionListener{
-	JLabel l;  
-    JRadioButton jb[] = new JRadioButton[5];  
-    JButton b1,b2;  
-    ButtonGroup bg;  
+	JLabel label;  
+    JRadioButton rdButtons[] = new JRadioButton[5];  
+    JButton button1, button2;  
+    ButtonGroup btGroup;  
     int count=0,current=0,x=1,y=1,now=0;  
     int m[]=new int[10];      
     
-    TestSystemInSwing(String s)  
+    TestSystemInSwing(String prgName)  
     {  
-        super(s);  
-        l = new JLabel();  
-        add(l);  
-        bg=new ButtonGroup();  
+        super(prgName);  
+        label = new JLabel();  
+        add(label);  
+        btGroup=new ButtonGroup();  
         for(int i=0;i<5;i++)  
         {  
-            jb[i]=new JRadioButton();     
-            add(jb[i]);  
-            bg.add(jb[i]);  
+            rdButtons[i]=new JRadioButton();     
+            add(rdButtons[i]);  
+            btGroup.add(rdButtons[i]);  
         }  
-        b1=new JButton("Next");  
-        b2=new JButton("Bookmark");  
-        b1.addActionListener(this);  
-        b2.addActionListener(this);  
-        add(b1);add(b2);  
+        button1=new JButton("Next");  
+        button2=new JButton("Bookmark");  
+        button1.addActionListener(this);  
+        button2.addActionListener(this);  
+        add(button1);add(button2);  
         set();  
-        l.setBounds(30,40,450,20);  
-        jb[0].setBounds(50,80,100,20);  
-        jb[1].setBounds(50,110,100,20);  
-        jb[2].setBounds(50,140,100,20);  
-        jb[3].setBounds(50,170,100,20);  
-        b1.setBounds(100,240,100,30);  
-        b2.setBounds(270,240,100,30);  
+        label.setBounds(30,40,450,20);  
+        rdButtons[0].setBounds(50,80,100,20);  
+        rdButtons[1].setBounds(50,110,100,20);  
+        rdButtons[2].setBounds(50,140,100,20);  
+        rdButtons[3].setBounds(50,170,100,20);  
+        button1.setBounds(100,240,100,30);  
+        button2.setBounds(270,240,100,30);  
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         setLayout(null);  
         setLocation(250,100);  
         setVisible(true);  
         setSize(600,350);  
     }  
+    
     public void actionPerformed(ActionEvent e)  
     {  
-        if(e.getSource()==b1)  
+        if(e.getSource()==button1)  
         {  
             if(check())  
                 count=count+1;  
@@ -53,8 +54,8 @@ public class TestSystemInSwing extends JFrame implements ActionListener{
             set();    
             if(current==9)  
             {  
-                b1.setEnabled(false);  
-                b2.setText("Result");  
+                button1.setEnabled(false);  
+                button2.setText("Result");  
             }  
         }  
         if(e.getActionCommand().equals("Bookmark"))  
@@ -68,7 +69,7 @@ public class TestSystemInSwing extends JFrame implements ActionListener{
             current++;  
             set();    
             if(current==9)  
-                b2.setText("Result");  
+                button2.setText("Result");  
             setVisible(false);  
             setVisible(true);  
         }  
@@ -96,88 +97,90 @@ public class TestSystemInSwing extends JFrame implements ActionListener{
             System.exit(0);  
         }  
     }  
+    
     void set()  
     {  
-        jb[4].setSelected(true);  
+        rdButtons[4].setSelected(true);  
         if(current==0)  
         {  
-            l.setText("Que1: Which one among these is not a primitive datatype?");  
-            jb[0].setText("int");jb[1].setText("Float");jb[2].setText("boolean");jb[3].setText("char");   
+            label.setText("Que1: Which one among these is not a primitive datatype?");  
+            rdButtons[0].setText("int");rdButtons[1].setText("Float");rdButtons[2].setText("boolean");rdButtons[3].setText("char");   
         }  
         if(current==1)  
         {  
-            l.setText("Que2: Which class is available to all the class automatically?");  
-            jb[0].setText("Swing");jb[1].setText("Applet");jb[2].setText("Object");jb[3].setText("ActionEvent");  
+            label.setText("Que2: Which class is available to all the class automatically?");  
+            rdButtons[0].setText("Swing");rdButtons[1].setText("Applet");rdButtons[2].setText("Object");rdButtons[3].setText("ActionEvent");  
         }  
         if(current==2)  
         {  
-            l.setText("Que3: Which package is directly available to our class without importing it?");  
-            jb[0].setText("swing");jb[1].setText("applet");jb[2].setText("net");jb[3].setText("lang");  
+            label.setText("Que3: Which package is directly available to our class without importing it?");  
+            rdButtons[0].setText("swing");rdButtons[1].setText("applet");rdButtons[2].setText("net");rdButtons[3].setText("lang");  
         }  
         if(current==3)  
         {  
-            l.setText("Que4: String class is defined in which package?");  
-            jb[0].setText("lang");jb[1].setText("Swing");jb[2].setText("Applet");jb[3].setText("awt");  
+            label.setText("Que4: String class is defined in which package?");  
+            rdButtons[0].setText("lang");rdButtons[1].setText("Swing");rdButtons[2].setText("Applet");rdButtons[3].setText("awt");  
         }  
         if(current==4)  
         {  
-            l.setText("Que5: Which institute is best for java coaching?");  
-            jb[0].setText("Utek");jb[1].setText("Aptech");jb[2].setText("SSS IT");jb[3].setText("jtek");  
+            label.setText("Que5: Which institute is best for java coaching?");  
+            rdButtons[0].setText("Utek");rdButtons[1].setText("Aptech");rdButtons[2].setText("SSS IT");rdButtons[3].setText("jtek");  
         }  
         if(current==5)  
         {  
-            l.setText("Que6: Which one among these is not a keyword?");  
-            jb[0].setText("class");jb[1].setText("int");jb[2].setText("get");jb[3].setText("if");  
+            label.setText("Que6: Which one among these is not a keyword?");  
+            rdButtons[0].setText("class");rdButtons[1].setText("int");rdButtons[2].setText("get");rdButtons[3].setText("if");  
         }  
         if(current==6)  
         {  
-            l.setText("Que7: Which one among these is not a class? ");  
-            jb[0].setText("Swing");jb[1].setText("Actionperformed");jb[2].setText("ActionEvent");  
-                        jb[3].setText("Button");  
+            label.setText("Que7: Which one among these is not a class? ");  
+            rdButtons[0].setText("Swing");rdButtons[1].setText("Actionperformed");rdButtons[2].setText("ActionEvent");  
+                        rdButtons[3].setText("Button");  
         }  
         if(current==7)  
         {  
-            l.setText("Que8: which one among these is not a function of Object class?");  
-            jb[0].setText("toString");jb[1].setText("finalize");jb[2].setText("equals");  
-                        jb[3].setText("getDocumentBase");         
+            label.setText("Que8: which one among these is not a function of Object class?");  
+            rdButtons[0].setText("toString");rdButtons[1].setText("finalize");rdButtons[2].setText("equals");  
+                        rdButtons[3].setText("getDocumentBase");         
         }  
         if(current==8)  
         {  
-            l.setText("Que9: which function is not present in Applet class?");  
-            jb[0].setText("init");jb[1].setText("main");jb[2].setText("start");jb[3].setText("destroy");  
+            label.setText("Que9: which function is not present in Applet class?");  
+            rdButtons[0].setText("init");rdButtons[1].setText("main");rdButtons[2].setText("start");rdButtons[3].setText("destroy");  
         }  
         if(current==9)  
         {  
-            l.setText("Que10: Which one among these is not a valid component?");  
-            jb[0].setText("JButton");jb[1].setText("JList");jb[2].setText("JButtonGroup");  
-                        jb[3].setText("JTextArea");  
+            label.setText("Que10: Which one among these is not a valid component?");  
+            rdButtons[0].setText("JButton");rdButtons[1].setText("JList");rdButtons[2].setText("JButtonGroup");  
+                        rdButtons[3].setText("JTextArea");  
         }  
-        l.setBounds(30,40,450,20);  
+        label.setBounds(30,40,450,20);  
         for(int i=0,j=0;i<=90;i+=30,j++)  
-            jb[j].setBounds(50,80+i,200,20);  
+            rdButtons[j].setBounds(50,80+i,200,20);  
     }  
+    
     boolean check()  
     {  
         if(current==0)  
-            return(jb[1].isSelected());  
+            return(rdButtons[1].isSelected());  
         if(current==1)  
-            return(jb[2].isSelected());  
+            return(rdButtons[2].isSelected());  
         if(current==2)  
-            return(jb[3].isSelected());  
+            return(rdButtons[3].isSelected());  
         if(current==3)  
-            return(jb[0].isSelected());  
+            return(rdButtons[0].isSelected());  
         if(current==4)  
-            return(jb[2].isSelected());  
+            return(rdButtons[2].isSelected());  
         if(current==5)  
-            return(jb[2].isSelected());  
+            return(rdButtons[2].isSelected());  
         if(current==6)  
-            return(jb[1].isSelected());  
+            return(rdButtons[1].isSelected());  
         if(current==7)  
-            return(jb[3].isSelected());  
+            return(rdButtons[3].isSelected());  
         if(current==8)  
-            return(jb[1].isSelected());  
+            return(rdButtons[1].isSelected());  
         if(current==9)  
-            return(jb[2].isSelected());  
+            return(rdButtons[2].isSelected());  
         return false;  
     }  
     
